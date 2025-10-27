e140449f1fc93a35cc0d1d34041b282b544e16b6
Jakub-Ko <92011164+Jakub-Ko@users.noreply.github.com>
2025-10-27 10:17:22 +0100

    Add files via upload

--- WinFormsApp2710/Form1.cs
+++ WinFormsApp2710/Form1.cs
@@ -3,11 +3,13 @@ namespace WinFormsApp2710
     public partial class Form1 : Form
     {
         /*List<int> lista = new List<int> {25, 83, 42, 31, 5};*/
+        Random r = new Random();
+        Lista lista = new();
 
         public Form1()
         {
             InitializeComponent();
-            Lista lista = new();
+            
             lista.Dodaj(25);
             lista.Dodaj(83);
             lista.Dodaj(42);
@@ -23,11 +25,22 @@ namespace WinFormsApp2710
                 temp = temp.next;
             }
             label1.Text = txt;
+
+            lista = new();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            lista.Dodaj(r.Next(1, 101));
+            Element temp = lista.head;
+            String txt = "To=";
+            while (temp != null)
+            {
+                txt += temp.wartoœæ.ToString();
+                txt += ", ";
+                temp = temp.next;
+            }
+            label1.Text = txt;
         }
     }
 }
